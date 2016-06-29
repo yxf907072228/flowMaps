7529a5fa86911d76c62125fddae8e1c63725534d
yexf <yexf@paratera.com>
2016-06-29 11:18:37 +0800

    修复火狐下面的bug

--- demos/demo.js
+++ demos/demo.js
@@ -2,15 +2,4 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import WorkFlow from '../src/workflow'
 
-
-class App extends React.Component{
-	
-	render(){
-		console.log(WorkFlow)
-		return (
-			<WorkFlow></WorkFlow>
-		)
-	}
-}
-
-ReactDom.render(<App />, document.getElementById('paper'));
+ReactDom.render(<WorkFlow />, document.getElementById('paper'));
